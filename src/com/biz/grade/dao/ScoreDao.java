86e804448d4008973d7a819cbe776aa610f39d56
ijs9200 <ijs9200@naver.com>
2019-02-10 20:02:00 +0900

    first

---
+++ src/com/biz/grade/dao/ScoreDao.java
@@ -0,0 +1,16 @@
+package com.biz.grade.dao;
+
+import java.util.*;
+
+public interface ScoreDao {
+
+//CRUD 선언
+	public int insert(ScoreVO vo);
+	
+	public List<ScoreVO> selectAll();
+	public ScoreVO findByNum(String sc_num);
+	
+	public int update(ScoreVO vo);
+	public int delete(String sc_num);
+		
+}
